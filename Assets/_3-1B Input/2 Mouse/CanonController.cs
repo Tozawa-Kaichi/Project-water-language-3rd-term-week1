e7dd40f36d0d5d402fb0063203ce45e3f61fbbb2
戸澤海智 <st37071556@icloud.com>
2021-10-13 15:24:02 +0900

    大砲

--- Assets/_3-1B Input/2 Mouse/CanonController.cs
+++ Assets/_3-1B Input/2 Mouse/CanonController.cs
@@ -2,6 +2,8 @@
 
 public class CanonController : MonoBehaviour
 {
+    [SerializeField] GameObject m_BombPrefab;
+    [SerializeField] Transform m_muzzle;
     void Start()
     {
         
@@ -13,5 +15,10 @@ public class CanonController : MonoBehaviour
         pos.z = 0;
         Vector3 dir = pos - this.transform.position;
         this.transform.right = dir;
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(m_BombPrefab,m_muzzle.position,this.transform.rotation);
+        }
     }
 }
