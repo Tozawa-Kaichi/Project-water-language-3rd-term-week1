4c8c24e7923a7a0a585e884c5d1e5ea1200049ac
戸澤海智 <st37071556@icloud.com>
2021-10-13 15:33:39 +0900

    大砲

---
+++ Assets/_3-1B Input/2 Mouse/BombController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    [SerializeField] float m_initialSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = this.transform.up * m_initialSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
