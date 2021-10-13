f95297848a2e1f893baf32943d9d5edcad998470
戸澤海智 <st37071556@icloud.com>
2021-10-13 14:36:08 +0900

    水曜日 言語 第一週 3term目

---
+++ Assets/_3-1B Input/1 WASD/SpaceShipController2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpaceShipController2D : MonoBehaviour
+{
+    [SerializeField] float _speed = 1f;
+    Rigidbody2D _rb = default;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(h,v);
+        _rb.velocity = dir.normalized * _speed;
+    }
+}
